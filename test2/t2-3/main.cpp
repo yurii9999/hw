678fd502cb261fe963a84b940585ef2648d51cfa
Yurii Kuzmin <yurii9kuzmin@gmail.com>
2017-12-15 16:20:46 +0300

    add t2-3

---
+++ test2/t2-3/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+
+    bool isComment = false;
+    char currentChar = 0;
+
+    while (in.peek() != EOF)
+    {
+        in.get(currentChar);
+
+        if (isComment)
+        {
+            if (currentChar != '\n' && currentChar != EOF)
+                cout << currentChar;
+            else
+            {
+                isComment = false;
+                cout << "\n";
+            }
+        }
+        else
+        {
+            if (currentChar == ';')
+            {
+                cout << ";";
+                isComment = true;
+            }
+        }
+    }
+
+    in.close();
+    return 0;
+}
