49772498f7fce76478f6af5e0ebdb5d32fbf35cb
Yurii Kuzmin <yurii9kuzmin@gmail.com>
2017-12-25 19:14:20 +0300

    add p7-1

---
+++ hw7/p7-1/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "tree.h"
+
+using namespace std;
+
+int main()
+{
+    Tree* tree = createTree();
+
+    cout << "\nEnter 0 to quit";
+    cout << "\nEnter 1 to add element";
+    cout << "\nEnter 2 to remove element";
+    cout << "\nEnter 3 to find element";
+    cout << "\nEnter 4 to print tree";
+    cout << "\nEnter 5 to print tree lowest..biggest";
+    cout << "\nEnter 6 to print tree biggest..lowest\n";
+
+    int c = 0;
+
+    do
+    {
+        cout << "Enter c: ";
+        cin >> c;
+
+        int element = 0;
+        switch (c)
+        {
+        case 1:
+            cout << "Enter element: ";
+            cin >> element;
+            add(tree, element);
+            break;
+        case 2:
+            cout << "Enter element: ";
+            cin >> element;
+            remove(tree, element);
+            break;
+        case 3:
+            cout << "Enter element: ";
+            cin >> element;
+            if (contains(tree, element))
+                cout << "it included";
+            else
+                cout << "it is not included";
+            break;
+        case 4:
+            print(tree);
+            cout << "\n";
+            break;
+        case 5:
+            printLowestFirst(tree);
+            cout << "\n";
+            break;
+        case 6:
+            printBiggestFirst(tree);
+            cout << "\n";
+            break;
+        }
+    }
+    while (c != 0);
+
+    deleteTree(tree);
+
+    return 0;
+}
