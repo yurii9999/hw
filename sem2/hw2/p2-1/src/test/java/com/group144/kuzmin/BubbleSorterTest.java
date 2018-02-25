a424730a8bee2dfbe100cbf0c648e1a569b0a38b
Yurii Kuzmin <yurii9kuzmin@gmail.com>
2018-02-25 18:00:22 +0300

    add p2-1

---
+++ sem2/hw2/p2-1/src/test/java/com/group144/kuzmin/BubbleSorterTest.java
@@ -0,0 +1,29 @@
+package com.group144.kuzmin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BubbleSorterTest {
+    @Test
+    public void sort() {
+        Integer[] array = {1, 3, 4, 2, 0};
+        Integer[] sorted = {0, 1, 2, 3, 4};
+
+        Sorter<Integer> sorter = new BubbleSorter<>();
+        sorter.sort(array);
+
+        assertArrayEquals(array, sorted);
+    }
+
+    @Test
+    public void oneDifferent() {
+        Integer[] array = {1, 5, 4, 9, 11};
+        Integer[] sorted = {1, 4, 5, 9, 11};
+
+        Sorter<Integer> sorter = new BubbleSorter<>();
+        sorter.sort(array);
+
+        assertArrayEquals(array, sorted);
+    }
+}
