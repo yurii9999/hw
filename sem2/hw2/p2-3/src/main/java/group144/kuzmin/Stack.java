87a13c37bf479ad0a4c7ae293350b0c2afea6da5
Yurii Kuzmin <yurii9kuzmin@gmail.com>
2018-05-01 09:33:30 +0300

    other p's removed

--- sem2/hw2/p2-3/src/main/java/group144/kuzmin/Stack.java
+++
@@ -1,9 +0,0 @@
-package group144.kuzmin;
-
-import java.util.EmptyStackException;
-
-public interface Stack<T> {
-    void push(T value);
-    T pop() throws EmptyStackException;
-    boolean isEmpty();
-}
